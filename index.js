073fabbe0a1bb3b18413e443aa7215a8089ef9c1
Alexandr <vashchekin01@mail.ru>
2022-06-06 21:07:15 +0300

    refac: porting some features

--- index.js
+++ index.js
@@ -1,85 +1,103 @@
-const inputAddTask = document.querySelector('.to-do-list__input_type_add-task');
-const buttonSubmitAddTask = document.querySelector('.to-do-list__button_type_add-task');
-const taskList = document.querySelector('.to-do-list__task-list');
+const inputAddTask = document.querySelector(".to-do-list__input_type_add-task");
+const buttonSubmitAddTask = document.querySelector(
+  ".to-do-list__button_type_add-task"
+);
+const taskList = document.querySelector(".to-do-list__task-list");
 
-const taskItemTemplate = document.querySelector('#task-item-template').content;
+const taskItemTemplate = document.querySelector("#task-item-template").content;
 
-let valueInputAddTask = '';
+let valueInputAddTask = "";
 
 let tasks = [
-  { id: 1, text: 'Полить цветы' },
-  { id: 2, text: 'Сходить в магазин' },
-]
+  { id: 1, text: "Полить цветы" },
+  { id: 2, text: "Сходить в магазин" },
+];
+
+// ---------------- ФУНКЦИИ ----------------
 
 const handleAddNewTask = (text, id) => {
-  const newTaskItem = taskItemTemplate.querySelector('.to-do-list__task-item').cloneNode(true);
-  const taskItemInput = newTaskItem.querySelector('.to-do-list__input_task-item');
-  const taskItemNumber = newTaskItem.querySelector('.to-do-list__task-item-number');
-  const taskItemEditButton = newTaskItem.querySelector('.to-do-list__task-item-button_type_edit');
-  const taskItemDeleteButton = newTaskItem.querySelector('.to-do-list__task-item-button_type_delete');
+  const newTaskItem = taskItemTemplate.querySelector(".to-do-list__task-item").cloneNode(true);
+  const taskItemInput = newTaskItem.querySelector(".to-do-list__input_task-item");
+  const taskItemNumber = newTaskItem.querySelector(".to-do-list__task-item-number");
+  const taskItemEditButton = newTaskItem.querySelector(".to-do-list__task-item-button_type_edit");
+  const taskItemDeleteButton = newTaskItem.querySelector(".to-do-list__task-item-button_type_delete");
 
-  taskItemDeleteButton.addEventListener('click', (event) => {
+  taskItemDeleteButton.addEventListener("click", (event) => {
     event.preventDefault();
     newTaskItem.remove();
 
-    tasks = tasks.filter((item) => { return Number(taskItemNumber.textContent) !== item.id });
-    tasks.forEach((item, index) => { item.id = index + 1 })
-
-    const currentListTaskItems = document.querySelectorAll('.to-do-list__task-item');
-    currentListTaskItems.forEach((item, index) => {
-      item.querySelector('.to-do-list__task-item-number').textContent = index + 1;
+    tasks = tasks.filter((item) => {
+      return Number(taskItemNumber.textContent) !== item.id;
+    });
+    tasks.forEach((item, index) => {
+      item.id = index + 1;
     });
 
-  })
+    const currentListTaskItems = document.querySelectorAll(".to-do-list__task-item");
+    currentListTaskItems.forEach((item, index) => {
+      item.querySelector(".to-do-list__task-item-number").textContent =
+        index + 1;
+    });
+  });
 
-  taskItemEditButton.addEventListener('click', (event) => {
+  let isClick = false;
+
+  // Выход из режима редактирования текста
+  const exitEditModeTaskItem = () => {
+    taskItemInput.setAttribute("disabled", true);
+    taskItemEditButton.classList.remove("active");
+    isClick = false;
+  };
+
+  // Выход из режима редактирования нажатием на клавиши Enter или Esc
+  const handleTaskItemInputKeyDown = (event) => {
+    if (event.code === "Enter" || event.code === "Escape") {
+      exitEditModeTaskItem();
+    }
+  };
+
+  taskItemEditButton.addEventListener("click", (event) => {
     event.preventDefault();
-
-    taskItemEditButton.classList.add('active');
-    taskItemInput.removeAttribute('disabled');
-    taskItemInput.focus();
-
-    taskItemInput.addEventListener('blur', () => {
-      taskItemInput.setAttribute('disabled', true);
-      taskItemEditButton.classList.remove('active');
-    }, {
+    isClick = !isClick;
+    if (isClick) {
+      taskItemInput.removeAttribute("disabled");
+      taskItemInput.focus();
+      taskItemEditButton.classList.add("active");
+    } else {
+      exitEditModeTaskItem();
+    }
+    taskItemInput.addEventListener("blur", exitEditModeTaskItem, {
       once: true,
     });
-
-    taskItemInput.addEventListener('keydown', (event) => {
-      if(event.code === 'Enter') {
-        taskItemInput.setAttribute('disabled', true);
-        taskItemEditButton.classList.remove('active');
-      }
-    })
-  })
+    taskItemInput.addEventListener("keydown", handleTaskItemInputKeyDown);
+  });
 
   taskItemInput.value = text;
   taskItemNumber.textContent = id;
 
   return newTaskItem;
-}
+};
 
-tasks.forEach((item, index) => {
+tasks.forEach((item) => {
   const text = item.text;
   const id = item.id;
   const newTask = handleAddNewTask(text, id);
   taskList.append(newTask);
-})
+});
 
-inputAddTask.addEventListener('input', (event) => {
+inputAddTask.addEventListener("input", (event) => {
   valueInputAddTask = event.target.value;
-})
+});
 
-buttonSubmitAddTask.addEventListener('click', (event) => {
+buttonSubmitAddTask.addEventListener("click", (event) => {
   event.preventDefault();
 
-  if(!valueInputAddTask) {
-    console.log('Поле пустое')
+  if (!valueInputAddTask) {
+    console.log("Поле пустое");
   } else {
-    let id  = tasks.length + 1;
+    let id = tasks.length + 1;
 
-    tasks.push({ id, text: valueInputAddTask })
+    tasks.push({ id, text: valueInputAddTask });
 
     const newTask = handleAddNewTask(valueInputAddTask, id);
 
@@ -87,8 +105,5 @@ buttonSubmitAddTask.addEventListener('click', (event) => {
 
     inputAddTask.value = "";
     valueInputAddTask = "";
-
   }
-  
-})
-
+});
